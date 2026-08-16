03c53c254bf14d1d650f85ecd04c94484eccbcb3
bgcode <violetcode@qq.com>
2026-08-16 09:23:02 +0800

    feat: 樱花特效默认开启，数量减少且速度调慢

--- src/config/effectsConfig.ts
+++ src/config/effectsConfig.ts
@@ -4,13 +4,13 @@ import type { SakuraConfig } from "../types/effectsConfig";
 
 export const sakuraConfig: SakuraConfig = {
 	// 是否启用樱花特效
-	enable: false,
+	enable: true,
 
 	// 是否允许用户在设置中切换
 	switchable: true,
 
 	// 樱花数量
-	sakuraNum: 21,
+	sakuraNum: 12,
 
 	// 樱花越界限制次数，-1为无限循环
 	limitTimes: -1,
@@ -36,21 +36,21 @@ export const sakuraConfig: SakuraConfig = {
 		// 水平移动
 		horizontal: {
 			// 水平移动速度最小值
-			min: -1.7,
+			min: -1.2,
 			// 水平移动速度最大值
-			max: -1.2,
+			max: -0.8,
 		},
 		// 垂直移动
 		vertical: {
 			// 垂直移动速度最小值
-			min: 1.5,
+			min: 1.0,
 			// 垂直移动速度最大值
-			max: 2.2,
+			max: 1.5,
 		},
 		// 旋转速度
-		rotation: 0.03,
+		rotation: 0.02,
 		// 消失速度，不应大于最小不透明度
-		fadeSpeed: 0.03,
+		fadeSpeed: 0.02,
 	},
 
 	// 层级，确保樱花在合适的层级显示
